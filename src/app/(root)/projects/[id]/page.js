0afc9d005f699ae00b1f7db76f8222da6aa14880
Pragnya sahu <pragnya110@gmail.com>
2024-03-27 14:56:11 +0530

    Project folder added

---
+++ src/app/(root)/projects/[id]/page.js
@@ -0,0 +1,5 @@
+"use client";
+import Projectdetails from "../../../Views/Projectdetails";
+export default function Projectdetailspage() {
+  return <Projectdetails />;
+}
---
+++ src/app/(root)/projects/page.js
@@ -0,0 +1,5 @@
+"use client";
+import Project from "../../Views/Projects";
+export default function Projectpage() {
+  return <Project />;
+}
