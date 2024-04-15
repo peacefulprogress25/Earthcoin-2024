51d8d8acd57d6204f97168adc95ca8e779bc3969
ChokkalingamS <lingam1707@gmail.com>
2024-04-15 19:51:47 +0530

    id updates

--- src/app/services/nexaflow.js
+++ src/app/services/nexaflow.js
@@ -2,6 +2,5 @@ import NexaflowInit from "nexaflow-web-sdk";
 import { envObj } from "../utils/env";
 
 const apiKey = envObj.nexaflow_api_key;
-console.log(apiKey);
 const nexaflowApi = new NexaflowInit(apiKey);
 export default nexaflowApi;
