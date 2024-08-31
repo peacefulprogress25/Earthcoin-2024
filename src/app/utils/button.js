4f07d4556f74430c618fae3b9aa1b63547ba1130
Chokkalingam.S <lingam1707@gmail.com>
2024-08-31 11:50:16 +0530

    button config

---
+++ src/app/utils/button.js
@@ -0,0 +1,30 @@
+export default {
+  home_earthNode_learnMore: {
+    title: "Learn more",
+    position: "Nodes",
+    page: "/",
+    external: false,
+    link: "",
+  },
+  home_earthNode_becomeNode: {
+    title: "Become a NODE",
+    position: "Nodes",
+    page: "/",
+    external: false,
+    link: "",
+  },
+  home_partners: {
+    title: "View Partners",
+    position: "Partners",
+    page: "/",
+    external: false,
+    link: "",
+  },
+  home_footer_banner: {
+    title: "$Earth GPT",
+    position: "AI Banner",
+    page: "/",
+    external: false,
+    link: "",
+  },
+};
