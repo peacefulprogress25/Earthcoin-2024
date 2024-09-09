479b00b1d0ac68511f851366b4e2379f2021eaf3
Muzzammil Hussain <095hussainmuzzammil@gmail.com>
2024-09-09 16:13:42 +0530

    resolve the design issue of book.

--- src/app/Views/Purpose/FlipBook.js
+++ src/app/Views/Purpose/FlipBook.js
@@ -3,63 +3,45 @@ import HTMLFlipBook from 'react-pageflip';
 import jungle from "../../../../public/assets/images/jungle.png";
 import space from "../../../../public/assets/images/space.png";
 import man from "../../../../public/assets/images/man.png";
-import ImageView from "../../Components/ImageView";
 import tree from "../../../../public/assets/images/tree.png";
+import ImageView from "../../Components/ImageView";
 
-// Page component
 const Page = React.forwardRef((props, ref) => {
   return (
     <div
-      className="demoPage"
+      className="demoPage p-5 flex flex-col justify-between bg-custom-gradient shadow-2xl  rounded-lg h-full w-full box-border z-20 opacity-100 transition-opacity duration-300 ease-out"
       ref={ref}
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'space-between',
-        padding: '20px',
-        backgroundColor: 'white',
-        boxShadow: '0 4px 10px rgba(0, 0, 0, 0.1)',
-        margin: '10px',
-        borderRadius: '8px',
-        height: '100%',
-        width: '100%',
-        overflow: 'hidden',
-        boxSizing: 'border-box',
-      }}
     >
       {props.children}
-      {/* <p className="text-right mt-2 font-bold">Page number: {props.number}</p> */}
     </div>
   );
 });
 
-// FlipBook component
 function FlipBook() {
   return (
     <HTMLFlipBook
-     width={500}
-      height={600}
-      maxShadowOpacity={0.5}
+      width={window.innerWidth < 1500 ? 150 : 550} 
+      height={window.innerWidth < 1500 ? 200 : 600} 
+      maxShadowOpacity={0.8}
       flippingTime={1000}
+      showCover={true}
+      className=" shadow-2xl" 
       style={{
-        boxShadow: '0 5px 20px rgba(0, 0, 0, 0.2)', 
+        boxShadow: '0 15px 15px rgba(0, 0, 0, 0.5)', 
+        perspective: "1500px",
         margin: '50px auto',
-        borderRadius: '10px',
-        backgroundColor: '#f0f0f0',
-        padding: '20px',
-        overflow: 'hidden',
+         background: "linear-gradient(to right, #ffffff, #f0f0f0)",
+        borderRadius: "10px"
       }}
     >
       {/* Page 1 */}
       <Page number="1">
-        <div className="w-full pr-4 pt-4">
-          <p className="text-[#101828] font-semibold text-left mt-2 text-[20px] sm:text-[28px] leading-6 sm:leading-7 font-syne">
-            Context before currency,
-            <span className="text-[#EC8000]"> Planet before profits</span>
-          </p>
-          <div className='p-4 mt-1'>
-            <p className='text-[#475467] text-left font-normal text-[15px] leading-6 sm:leading-7 font-inter'>
-              A cosmic satellite observing Sapiens on Earth would certainly
+        <p className="text-[#101828] font-semibold text-left text-[6px] sm:text-[30px] leading-2 sm:leading-9 font-syne">
+          Context before currency, <span className="text-[#EC8000]">Planet before profits</span>
+        </p>
+        <div>
+          <p className="text-[#475467] text-left font-normal text-[4.3px] sm:text-[15px] leading-2 sm:leading-7 font-inter mt-4">
+          A cosmic satellite observing Sapiens on Earth would certainly
               admit we have progressed leaps and bounds in eliminating
               existential threats and pushing the envelope of our
               consciousness. From developing language to communicate
@@ -78,18 +60,17 @@ function FlipBook() {
               <span className='text-[#EC8000] font-semibold'>
                 limited by our imagination and boundaries of science.
               </span>
-            </p>
-          </div>
+          </p>
         </div>
       </Page>
 
       {/* Page 2 - Image Content */}
       <Page number="2">
-        <div className="w-full pr-10 pt-4">
-          <div className="h-[230px] rounded-lg overflow-hidden"> {/* Increased height */}
+        <div className="sm:mb-6 sm:p-3 mb-4 p-2">
+          <div className="sm:h-[230px] sm:mb-4 h-auto mb-2 rounded-lg overflow-hidden">
             <ImageView src={jungle} alt="Jungle" width={1060} height={260} className="object-cover" />
           </div>
-          <div className="h-[230px] mt-2 rounded-lg overflow-hidden"> {/* Increased height */}
+          <div className="sm:h-[230px] h-auto rounded-lg overflow-hidden">
             <ImageView src={space} alt="Space" width={1060} height={260} className="object-cover" />
           </div>
         </div>
@@ -97,15 +78,14 @@ function FlipBook() {
 
       {/* Page 3 */}
       <Page number="3">
-        <div className='w-full'>
-          <p className='text-[#101828] font-semibold text-left mt-2 text-[20px] sm:text-[28px] leading-6 sm:leading-7 font-syne'>
-            The recently experienced exponential advances although are not
-            <span className='text-[#EC8000]'> decoupled</span> from its{" "}
-            <span className='text-[#EC8000]'>tradeoffs</span> -
-          </p>
-          <div className='p-4 mt-1'>
-            <p className='text-[#475467] text-left font-normal text-[15px] leading-6 sm:leading-7 font-inter'>
-              We have sacrificed our connection to the natural world,
+        <p className="text-[#101828] font-semibold text-left text-[6px] sm:text-[28px] leading-2 sm:leading-7 font-syne">
+          The recently experienced exponential advances although are not{" "}
+          <span className="text-[#EC8000]">decoupled</span> from its{" "}
+          <span className="text-[#EC8000]">tradeoffs</span> -
+        </p>
+        <div className="pt-2">
+          <p className="text-[#475467] text-left font-normal text-[5px] sm:text-[15px] leading-2 sm:leading-7 font-inter">
+          We have sacrificed our connection to the natural world,
               perceiving it as something that can be domesticated by human
               will (fairly succeeded at that too). <br /> <br />
               · Accepted incessant planning for the future as a given since
@@ -116,20 +96,18 @@ function FlipBook() {
               yrs back till date that has wiped out between 75-90 percent of
               species every time. 4 of those have been climate change
               induced.
-            </p>
-          </div>
+          </p>
         </div>
       </Page>
 
-      {/* Page 4 */}
+      {/* Page 4 - Image Content */}
       <Page number="4">
-        <div className="w-full pr-10 pt-4">
-          <div className="h-[260px] rounded-lg overflow-hidden"> {/* Increased height */}
+        <div className="p-3">
+          <div className="sm:h-[260px] sm:mb-4 mb-2 h-auto rounded-lg overflow-hidden">
             <ImageView src={man} alt="Man" width={960} height={260} className="object-cover" />
           </div>
-          <div className="p-4 mt-2">
-            <p className='text-[#475467] text-left font-normal text-[15px] leading-6 sm:leading-7 font-inter'>
-              The dissonance amongst humanity in recognizing the
+          <p className="text-[#475467] text-left font-normal sm:text-[15px] text-[4.5px] leading-2 sm:leading-7 font-inter">
+          The dissonance amongst humanity in recognizing the
               interconnectedness of life and that{" "}
               <b>
                 we are all simply matter processing information differently
@@ -140,66 +118,63 @@ function FlipBook() {
               thought that is assumed for &rsquo;reality&rsquo; in
               today&rsquo;s time. The quote that says it is easier to
               imagine the end of world than
-            </p>
-          </div>
+          </p>
         </div>
       </Page>
 
       {/* Page 5 */}
       <Page number="5">
-        <div className="w-full pt-4 pr-4">
-          <p className='text-[#475467] text-left font-normal text-[15px] leading-6 sm:leading-7 font-inter'>
+      <p className='text-[#475467] text-left font-normal sm:text-[15px] text-[5px] leading-2 sm:leading-7 font-inter'>
             Capitalism has never been more true. It is essential we
             understand that{" "}
             <span className='text-[#EC8000]'>
               our current superiority is small parts our effort and in large
               parts serendipity.
             </span>{" "}
+           
             Our perpetual superiority will be large parts effort and small
             parts luck. Hence tread carefully we must as we are yet to find
             a sign of intelligent life absolutely anywhere across the
             enormously chaotic universe we exist in. Even if we do, life as
             we know it is a planetary phenomenon, we will never feel
             completely at home anywhere else in the universe.
-            <br />
-            <br />
-            <p className='font-semibold text-[28px] font-syne text-black'>
+            </p>
+            {/* <br /> */}
+            {/* <br /> */}
+            <p className='font-semibold sm:text-[28px] text-[6px] leading-2 sm:leading-7 font-syne text-black'>
               The aforementioned tradeoffs or shifts in perception have now
               come seeking reparations. The Anthropocene era characterized
               by -
-              <br />
-              <br />
+              {/* <br /> */}
+              {/* <br /> */}
             </p>
+            <p className='sm:text-[15px] text-[5px] leading-2 sm:leading-7'>
             · Our unrestrained extraction of the decayed plant and animal
             matter converted to fuel over hundreds of millions of years
             being exposed to the atmosphere in a matter of mere centuries.
-          </p>
-        </div>
+            </p>
       </Page>
 
-      {/* Page 6 */}
+      {/* Page 6 - Image Content */}
       <Page number="6">
-        <div className="pt-4 pr-10">
-          <p className='text-[#475467] text-left font-normal text-[15px] leading-6 sm:leading-7 font-inter'>
-          
-                  · Our unhindered destruction of thriving natural ecosystems
+        <div className="p-3">
+          <p className="text-[#475467] text-left font-normal sm:text-[15px] text-[5px]  leading-2 sm:leading-7 font-inter">
+          · Our unhindered destruction of thriving natural ecosystems
                   for raw materials, grazing and agriculture. <br />
                   · Our infusion of deadly chemicals into the soil, air and
                   water for intensifying agricultural output. <br />· Our toxic
                   use and throw consumerism for exponential growth.
-                  </p>   
-                  <div className=" mt-4 pr-4 h-[430px] rounded-lg overflow-hidden"> {/* Increased height */}
+          </p>
+          <div className="sm:h-[430px] sm:mt-4 mt-2 h-auto rounded-lg overflow-hidden">
             <ImageView src={tree} alt="Tree" width={960} height={260} className="object-cover" />
           </div>
-          
         </div>
       </Page>
 
       {/* Page 7 */}
       <Page number="7">
-        <div className="p-4">
-          <p className='text-[#475467] text-left font-normal text-[15px] leading-6 sm:leading-7 font-inter'>
-            The massive rise in our standards of living at nature&rsquo;s
+        <p className="text-[#475467] text-left font-normal sm:text-[15px] text-[4.5px] leading-2 sm:leading-7 font-inter">
+        The massive rise in our standards of living at nature&rsquo;s
             expense has now come claiming the debt in the face of climate
             change.{" "}
             <br />
@@ -224,28 +199,26 @@ function FlipBook() {
             temperatures to unbreathable air. These elements of chaos are
             fully capable of making the COVID pandemic feel like a ripple
             in a tsunami. Waves of which we have already
-          </p>
-        </div>
+        </p>
       </Page>
 
       {/* Page 8 */}
       <Page number="8">
-        <div className="pr-10 pt-4">
-          <p className='text-[#475467] text-left font-normal text-[15px] leading-6 sm:leading-7 font-inter'>
+      <p className='text-[#475467] text-left font-normal sm:text-[15px] text-[5px] leading-2 sm:leading-7 font-inter'>
             Begun witnessing and these consequences are still simply what
             is within our scope of comprehension, unforeseen feedback
             loops are the icing on the proverbial multi-layered climate
             change cake.
           </p>{" "}
           <br />
-          <p className='font-semibold text-[22px] font-syne text-black'>
+          <p className='font-semibold sm:text-[22px] text-[5px] leading-2 sm:leading-7 font-syne text-black'>
             The onslaught of major environmental issues is a reflection of
             the deep systemic fault lines that exist within our ideology
             of sustaining life currently.
             <br />
             <br />
           </p>
-          <p className='text-[#475467] text-left font-normal text-[15px] leading-6 sm:leading-7 font-inter'>
+          <p className='text-[#475467] text-left font-normal sm:text-[15px] text-[5px] leading-2 sm:leading-7 font-inter'>
             Also before we begin to rationalize climate changes on
             geological timelines it behooves us to keep in mind that there
             weren&rsquo;t over 7 going to 9 billion humans breathing on
@@ -254,14 +227,12 @@ function FlipBook() {
             perils of climate change too are unevenly distributed. It is
             now a commonly accepted premise that
           </p>{" "}
-        </div>
       </Page>
 
       {/* Page 9 */}
       <Page number="9">
-        <div className="p-4">
-          <p className='text-[#475467] text-left font-normal text-[15px] leading-6 sm:leading-7 font-inter'>
-            The part of humanity that is largely responsible for the
+        <p className="text-[#475467] text-left font-normal sm:text-[15px] text-[4.5px] leading-2 sm:leading-7 font-inter">
+        The part of humanity that is largely responsible for the
             predicament at hand is also better equipped to deal with the
             problem while the lower rungs of society will be left gasping
             as usual. Un/Fortunately cyclones, fires, viruses, and floods
@@ -286,15 +257,13 @@ function FlipBook() {
             only possible when the environment is conducive towards it.
             Not when every action undertaken to date by 21st-century
             humans is in direct detriment of the planet we inhabit.
-          </p>
-        </div>
+        </p>
       </Page>
 
       {/* Page 10 */}
       <Page number="10">
-        <div className="pr-10 pt-4">
-          <p className='text-[#475467] text-left font-normal text-[15px] leading-6 sm:leading-7 font-inter'>
-            Although we see governments and corporates try to take
+        <p className="text-[#475467] text-left font-normal sm:text-[15px] text-[4.8px] leading-2 sm:leading-7 font-inter">
+        Although we see governments and corporates try to take
             &rsquo;bold&rsquo; action to ward off the worst of climate
             change, it&rsquo;s like Einstein said - it is not possible to
             solve a problem from the same level of frequency that created
@@ -321,11 +290,11 @@ function FlipBook() {
               {" "}
               Creating resonance between the two is the only way forward.
             </b>
-          </p>
-        </div>
+        </p>
       </Page>
     </HTMLFlipBook>
   );
 }
 
 export default FlipBook;
+
