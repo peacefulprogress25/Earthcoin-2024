378e3ffa97c9e87234d935d675a56469773b6e49
Vishal Patel <85049843+venteury@users.noreply.github.com>
2024-09-17 17:24:18 +0530

    changes

--- src/app/Components/GetInTouch.js
+++ src/app/Components/GetInTouch.js
@@ -21,12 +21,12 @@ export default function GetInTouch() {
         team.
       </p>
       <Link
-          className="w-[100px] mt-4 text-white font-inter flex h-10 items-center justify-center rounded-md bg-[#EC8000] p-2 cursor-pointer text-sm"
-          href={buttonConfig?.faq_footer_banner?.link || ""}
-          target={buttonConfig?.faq_footer_banner?.external ? "_blank" : "_self"}
-        >
-          {buttonConfig?.faq_footer_banner?.title}
-        </Link>
+        className="w-[100px] mt-4 !text-white font-inter flex h-10 items-center justify-center rounded-md bg-[#EC8000] p-2 cursor-pointer text-sm !no-underline" 
+        href={buttonConfig?.faq_footer_banner?.link || ""}
+        target={buttonConfig?.faq_footer_banner?.external ? "_blank" : "_self"}
+      >
+        {buttonConfig?.faq_footer_banner?.title}
+      </Link>
       {/* <button className="w-[100px] mt-4 text-white font-inter flex h-10 items-center justify-center rounded-md bg-[#EC8000] p-2 cursor-pointer text-sm">
         Get in touch
       </button> */}
