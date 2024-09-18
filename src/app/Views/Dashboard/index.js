c3124a8265cfb506c229e2dee2216386fc1a4ca0
Vishal Patel <85049843+venteury@users.noreply.github.com>
2024-09-18 14:38:57 +0530

    changes

--- src/app/Views/Dashboard/index.js
+++ src/app/Views/Dashboard/index.js
@@ -243,7 +243,7 @@ export default function Dashboard() {
   ];
   return (
     <div>
-      <div className="mt-32 w-full max-w-screen-2xl mx-auto px-4 sm:px-[6%] flex gap-10 flex-col items-center pb-10">
+      <div className="mt-32 w-full max-w-screen-2xl  px-18 sm:px-[6%] flex gap-10 flex-col items-center pb-10">
         <div className="flex flex-col items-center gap-2">
           <p className="text-[#EC8000] font-semibold text-center text-[14px] font-inter">
             Dashboard
