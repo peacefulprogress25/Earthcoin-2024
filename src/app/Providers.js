11b5b1ed90bc71ec0e39f70b6699795ef4293475
Chokkalingam.S <lingam1707@gmail.com>
2024-10-23 12:10:26 +0530

    google analytics addition

--- src/app/Providers.js
+++ src/app/Providers.js
@@ -1,14 +1,31 @@
 "use client";
 
 import { Toaster } from "sonner";
-
+import ReactGA from "react-ga4";
 import { Provider as ReduxProvider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistStore } from "redux-persist";
 import { store } from "./redux";
+import { usePathname } from "next/navigation";
+import { useEffect } from "react";
+import { envObj } from "./utils/env";
 export const persistor = persistStore(store);
 
 export default function Providers({ children }) {
+  const analyticsId = envObj.ga_track_id;
+  const pathname = usePathname();
+
+  useEffect(() => {
+    const googleAnalytics = async () => {
+      ReactGA.initialize(analyticsId);
+      ReactGA.send({
+        hitType: "pageview",
+        page: pathname,
+        title: pathname,
+      });
+    };
+    googleAnalytics();
+  }, [pathname]);
   return (
     <ReduxProvider store={store}>
       <PersistGate loading={false} persistor={persistor}>
